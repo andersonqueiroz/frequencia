4dbbbe22b0d0a7132aacc5ea3fa5c5ec875b516f
Anderson Queiroz <andersonkeiroz@gmail.com>
2022-02-11 14:49:52 -0300

    Adiciona CSRF_TRUSTED_ORIGINS no settings

--- frequencia/settings.py
+++ frequencia/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
-
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default=[], cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
