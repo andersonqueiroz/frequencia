add604044fc3b2aa4bfcefcc60bf576d9a8ea058
Anderson Queiroz <andersonkeiroz@gmail.com>
2022-08-05 14:16:11 -0300

    Ajusta DEFAULT_RENDERER_CLASSES do DRF

--- frequencia/settings.py
+++ frequencia/settings.py
@@ -140,6 +140,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 }
 
 
